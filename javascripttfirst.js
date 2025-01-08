1995547238868d6e750ca9bc593cc3d697c4425e
VUPPALA  AKSHAY <akshay.dunny08@gmail.com>
2025-01-08 21:27:01 +0530

    Add files via upload

---
+++ javascripttfirst.js
@@ -0,0 +1,45 @@
+/*
+// OBJECTS AND VARIABLES
+const profile = 
+{
+    name : "AKSHAY",
+    posts : 195,
+    followers : 569,
+    following : 1100,
+    follow: false,
+}
+console.log(profile);
+console.log(typeof profile["name"]);
+*/
+// PROMPT AND IFELSE (INPUT)
+
+
+/*
+let nam = prompt("ENTER THE NUMBER");
+if(nam % 5 == 0)
+{
+    console.log(nam,"IS MULTIPLE OF 5");
+}
+else
+{
+    console.log(nam, " IS NOT A MULTIPLE OF 5");
+}
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
