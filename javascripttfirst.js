48eb62fc4e26803cf2a9febc04c24b1ae0870b28
VUPPALA  AKSHAY <akshay.dunny08@gmail.com>
2025-05-11 11:58:56 +0530

    Add files via upload

--- javascripttfirst.js
+++ javascripttfirst.js
@@ -1,15 +1,24 @@
-/*
+
 // OBJECTS AND VARIABLES
+/*
 const profile = 
 {
-    name : "AKSHAY",
+    namee : "AKSHAY",
     posts : 195,
     followers : 569,
     following : 1100,
     follow: false,
 }
+
+profile["posts"] = profile["posts"] + 1;
+
+profile["namee"]  =  "rahul";
+console.log(profile["namee"]);
+console.log(profile["posts"]);
+
 console.log(profile);
-console.log(typeof profile["name"]);
+
+
 */
 // PROMPT AND IFELSE (INPUT)
 
@@ -26,20 +35,36 @@ else
 }
 
 
-
+console.log("helhlo")
 
 */
 
+//  GRADE CODE
+// let s =prompt("Enter your score");
 
 
+// if ( s >= 80 && s <= 100 )
+// {
+//     console.log("GRADE A");
+// }
+// else if ( s >= 70 && s <= 89)
+// {
+//     console.log("B");
+// }
+//  else if(s >= 60 && s <= 69)
+//  {
+//     console.log("C");   
+//  }
 
+//  else if ( s >= 0 && s <= 49)
+//  {
+//     console.log("fail"); 
+//  }
 
+let arr = [2,2,3,4,5];
 
+let newArr = arr.filter((val) => {
+    return val + 2 == 4;
+});
 
-
-
-
-
-
-
-
+console.log(newArr);
